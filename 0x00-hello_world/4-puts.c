b2f74125b3f5a0b8b7aa6ac4a0402ed5742e7a01
Alexoat76 <3915@holbertonschool.com>
2021-09-30 19:18:52 +0000

    First prog. Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+* File: 4-puts.c
+* Author: Alex Arévalo
+* e-mail: 3915@holbertonschool.com
+*/
+
+#include <stdio.h>
+/*
+*C program print exactly "Programming is like building a multilingual puzzle,followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
