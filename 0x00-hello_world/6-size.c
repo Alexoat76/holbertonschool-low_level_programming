d57f32c66ebcdbd7e6c67b91541eaf4b590669c4
Alexoat76 <3915@holbertonschool.com>
2021-09-30 21:20:33 +0000

    Size types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-size.
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types on the computer
+ * it is compiled and run on
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+return (0);
+}
