7741f9582acfe8522fde9cbb45febcc4953a3184
Alexoat76 <3915@holbertonschool.com>
2021-09-30 19:38:24 +0000

    First Prog. Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * e-mail: 3915@holbertonschool.com
  */
 /*
-*C program print exactly "Programming is like building
+*main - C program print exactly "Programming is like building
  a multilingual puzzle,followed by a new line.
  * Return: Always 0.
  */
