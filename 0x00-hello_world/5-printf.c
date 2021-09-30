da94431601bec828399c149a693689a8cdfc47ad
Alexoat76 <3915@holbertonschool.com>
2021-09-30 19:59:28 +0000

    Firts printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+/*
+ * File: 4-puts.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
