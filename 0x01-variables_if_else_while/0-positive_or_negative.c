c69eacf8b149c6681afcaa7999b4f90c44c0e28f
Alexoat76 <3915@holbertonschool.com>
2021-10-01 21:36:04 +0000

    Decl_if_elseif_else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,16 +23,16 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%i is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 (n < 0)
 {
-printf("%i is negative\n", n);
+printf("%d is negative\n", n);
 }
 
 return (0);
