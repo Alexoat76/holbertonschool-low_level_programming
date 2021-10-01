9c967f0cacec68d215c4dd11f33acaf6b4066156
Alexoat76 <3915@holbertonschool.com>
2021-10-01 01:17:52 +0000

    function write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+/*
+ * File: 101-quote.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+
+write(1, "and prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
