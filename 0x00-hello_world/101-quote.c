84897a09f2454815bf9b48a35a10c26070a24c81
Alexoat76 <3915@holbertonschool.com>
2021-10-01 01:35:48 +0000

    function write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,13 +8,14 @@
 #include <unistd.h>
 
 /**
- * Description: Prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - Prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
  *
  * Return: 1
  */
 
 int main(void)
 {
-write(1, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
