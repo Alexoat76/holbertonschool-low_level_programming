df6ae5ee0e29ac4b0190a10083a3a1704c476d98
Alexoat76 <3915@holbertonschool.com>
2021-10-01 22:20:44 +0000

    Decl_if_elseif_else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int n;
 char last[] = "Last digit of";
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
+printf("%s %d is %d and is ", last, n, n % 10);
 if (n % 10 > 5)
 {
 printf("greater than 5\n");
