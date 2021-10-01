9191af0621e8f3542304085716354e5e877736cb
Alexoat76 <3915@holbertonschool.com>
2021-10-01 18:31:31 +0000

    Decl_if_elseif_else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -26,18 +26,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-if else(n > 0)
+if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else
-(n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
+else
+{
+printf("%d is zero\n", n);
 }
+
 return (0);
 }
