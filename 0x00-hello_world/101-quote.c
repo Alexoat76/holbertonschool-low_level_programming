9d20e31cedd2cd187c4134b4ca95c9a0dc01dba4
Alexoat76 <3915@holbertonschool.com>
2021-10-01 01:28:33 +0000

    function write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,6 +15,6 @@
 
 int main(void)
 {
-write(1, "and prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+write(1, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
