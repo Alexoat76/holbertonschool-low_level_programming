7b737d4332c288a1557a1771b0a460cc28c8d52e
Alexoat76 <3915@holbertonschool.com>
2021-10-01 23:08:36 +0000

    function_putchar_3times

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase and uppercase.
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char letter;
+char Letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar (letter);
+for (Letter = 'A'; Letter <= 'Z'; Letter++)
+putchar (Letter);
+putchar ('\n');
+
+return (0);
+}
