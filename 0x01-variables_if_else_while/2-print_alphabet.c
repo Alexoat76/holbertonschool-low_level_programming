a175eaa537117e19c8e74bd14dc78f3e9af1077a
Alexoat76 <3915@holbertonschool.com>
2021-10-01 22:40:45 +0000

    putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase.
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar (letter);
+
+return (0);
+}
