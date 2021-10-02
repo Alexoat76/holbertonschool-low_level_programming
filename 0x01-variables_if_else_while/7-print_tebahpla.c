bcd1305dcb92fc57e92b406505e1b1976f5c74d7
Alexoat76 <3915@holbertonschool.com>
2021-10-02 02:21:27 +0000

    decrease alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the lowercase alphabet in reverse.
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar (letter);
+putchar ('\n');
+
+return (0);
+}
