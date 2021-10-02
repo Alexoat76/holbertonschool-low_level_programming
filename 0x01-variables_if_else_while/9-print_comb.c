c43e34c967122235cd91aa29f985ae0e8acebd25
Alexoat76 <3915@holbertonschool.com>
2021-10-02 03:01:28 +0000

    function_putchar_comb_num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints all possible combinations of single-digit numbers.
+* starting from 0
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 9; n++)
+{
+putchar((n % 10) + '0');
+if (n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
