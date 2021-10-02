5ffd76022843fa2e48f56e6d1e7ad3ddcf7b924e
Alexoat76 <3915@holbertonschool.com>
2021-10-02 02:06:22 +0000

    num_funct_putchar_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+/*
+ * File: 6-print_numberz.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the single digit numbers of base 10
+* starting from 0
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+putchar (n);
+n++;
+}
+putchar('\n');
+return (0);
+}
