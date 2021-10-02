3c2b19f869c47ebc7d22c5c4269845943899774c
Alexoat76 <3915@holbertonschool.com>
2021-10-02 02:44:25 +0000

    Decl_for_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_base16.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints all the numbers of base 16 in lowercase
+* starting from 0
+* Return: Always 0.
+*/
+
+int main(void)
+{
+  int n;
+  char letter;
+
+for (n = 0; n < 10; n++)
+putchar((n % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+
+return (0);
+}
