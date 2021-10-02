7ea1aaf349854936fef223b97d2a35feeac272e0
Alexoat76 <3915@holbertonschool.com>
2021-10-02 04:49:35 +0000

    comb_3digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,19 +22,17 @@ for (n = 0; n < 8; n++)
 for (m = n + 1; m < 9; m++)
 {
 for (p = m + 1; p < 10; p++)
+{
 putchar((n % 10) + '0');
 putchar((m % 10) + '0');
 putchar((p % 10) + '0');
 if (n == 7 && m == 8 && p == 9)
 continue;
-
 putchar(',');
 putchar(' ');
-
 }
 }
 }
-
 putchar('\n');
 
 return (0);
