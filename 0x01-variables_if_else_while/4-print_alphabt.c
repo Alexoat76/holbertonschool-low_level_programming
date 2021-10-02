ddbd1646d6976d16c7b2d1140b3e5f07826b84b5
Alexoat76 <3915@holbertonschool.com>
2021-10-02 01:30:43 +0000

    Operators_Logic_Not_Logic

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/*
+ * File: 4-print_alphabt.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase but except q and e
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar (letter);
+}
+putchar ('\n');
+
+return (0);
+}
