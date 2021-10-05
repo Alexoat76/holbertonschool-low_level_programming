0aad47d745edaf6539156ca0a87a04d90abf5bce
Alexoat76 <3915@holbertonschool.com>
2021-10-05 19:18:34 +0000

    Check_lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,7 +7,7 @@
 #include "main.h"
 
 /**
-* _islower: Checks for lowercase character.
+* _islower - Checks for lowercase character.
 * @c: character to be checked.
 * Return: 1 if c is lowercase, 0 otherwise
 */
