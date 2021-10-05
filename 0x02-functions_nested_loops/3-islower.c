b90554b3bcbf23b0543b9e89ff78e9f58299fcb2
Alexoat76 <3915@holbertonschool.com>
2021-10-05 19:12:25 +0000

    check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-islower.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* _islower: Checks for lowercase character.
+* @c: character to be checked.
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar ('\n');
+}
