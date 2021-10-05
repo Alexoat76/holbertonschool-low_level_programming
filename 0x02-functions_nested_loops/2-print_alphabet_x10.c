23fca0352ec26b60bc218a0a0b572930ab30312b
Alexoat76 <3915@holbertonschool.com>
2021-10-05 18:21:31 +0000

    loop_while_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-putchar.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints 10 times the alphabet in lowercase,
+* followed by a new line.
+*/
+
+void print_alphabet_x10(void)
+{
+int count = 0;
+char c;
+while (count++ <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar ('\n');
+}
+}
