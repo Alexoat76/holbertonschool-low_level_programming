723ea0ec12420e675e0d247fb9ac6bd51e2d58f7
Alexoat76 <3915@holbertonschool.com>
2021-10-06 00:22:31 +0000

    print_hours:minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-24_hours.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* jack_bauer - Prints every minute of the day of Jack Bauer,
+*              starting from 00:00 to 23:59.
+*/
+
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
+}
