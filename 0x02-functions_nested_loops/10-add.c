5b1a6b63a6dab98f11ed4d283126fac6da88494e
Alexoat76 <3915@holbertonschool.com>
2021-10-06 02:32:23 +0000

    add_integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: 10-add.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* add - Adds two integers and returns the result.
+* @n: first integer to be add.
+* @m: second integer to be add.
+* Return: result of addition.
+*/
+
+int add(int n, int m)
+{
+return (n + m);
+}
