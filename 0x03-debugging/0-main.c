7adb0f978d97efcb5ee6c7618d3e530fb4fb3066
Alexoat76 <3915@holbertonschool.com>
2021-10-06 21:10:40 +0000

    test positive/negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-putchar.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
