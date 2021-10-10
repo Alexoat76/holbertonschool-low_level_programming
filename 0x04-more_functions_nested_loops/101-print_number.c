120c9ab1784d39dd7a6586236ca764f3b3b94280
Alexoat76 <3915@holbertonschool.com>
2021-10-10 04:49:05 +0000

    print_num

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+/*
+ * File: 101-print_number.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_triangle - Prints an integer.
+* @n: integer to be printed.
+*/
+
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+printf(num / 10);
+_putchar((num % 10) + '0');
+}
