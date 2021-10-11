a538adb48e775a4311eb2f57bde884e89ca8327c
Alexoat76 <3915@holbertonschool.com>
2021-10-11 07:07:47 +0000

    fix_print_integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+/*
+ * File: 101-print_number.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_number - Prints an integer.
+* @n: integer to be printed.
+*/
+
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
