a315428db7557d2d70a92a41d6b89b87ea972034
Alexoat76 <3915@holbertonschool.com>
2021-10-11 05:29:11 +0000

    print_num_ten_times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+/*
+ * File: 5-more_numbers.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+* Return: Always 0.
+*/
+
+void more_numbers(void)
+{
+int n, m;
+for (n = 0; n < 10; n++)
+{
+for (m = 0; m < 15; m++)
+{
+if (m / 10 != 0)
+{
+_putchar(m / 10 + '0');
+}
+_putchar(m % 10 + '0');
+}
+_putchar('\n');
+}
+}
