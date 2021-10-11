97aee5314ea8bccde44dd2cdd17b344cc9444837
Alexoat76 <3915@holbertonschool.com>
2021-10-11 05:04:09 +0000

    print_num_$

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-print_numbers.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_numbers - Prints the numbers, from 0 to 9,
+* followed by a new line.
+* Return: Always 0.
+*/
+
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+_putchar((n % 10) + '0');
+_putchar('\n');
+}
