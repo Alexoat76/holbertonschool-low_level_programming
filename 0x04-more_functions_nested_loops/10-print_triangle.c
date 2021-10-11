600703d311dde55ec5257f1df14c1a016c4a6e8b
Alexoat76 <3915@holbertonschool.com>
2021-10-11 06:41:26 +0000

    print_triangles_#

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+/*
+ * File: 10-print_triangle.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_triangle - Prints a triangle with "#" character,
+* followed by a new line.
+* @size : size of triangle.
+*/
+
+void print_triangle(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if (j <= size - i)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
