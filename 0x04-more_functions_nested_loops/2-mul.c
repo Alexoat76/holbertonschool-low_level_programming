9cc33c79d0c9589b6cec7356174f5e33125c630f
Alexoat76 <3915@holbertonschool.com>
2021-10-11 04:53:41 +0000

    Proto_mul_integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* mul - Multiples two integers.
+* @a: First integer to be multiplied.
+* @b: Second integer to be multiplied.
+* Return: The result of multiplication.
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
