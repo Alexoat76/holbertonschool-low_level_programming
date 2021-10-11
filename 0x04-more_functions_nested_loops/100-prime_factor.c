e34e84f6d20878658f8cef612a40114bf42a29c7
Alexoat76 <3915@holbertonschool.com>
2021-10-11 06:56:14 +0000

    prime_factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+/*
+ * File: 100-prime_factor.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the largest prime
+ *        factor of the number 612852475143.
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+long i;
+long num = 612852475143;
+for (i = 2; i < num; i++)
+{
+if (num % i == 0)
+num /= i;
+}
+printf("%li\n", num);
+return (0);
+}
