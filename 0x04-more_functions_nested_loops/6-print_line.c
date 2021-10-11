8b8f9ba654b6954b95a7731a82e474a028d1edda
Alexoat76 <3915@holbertonschool.com>
2021-10-11 05:39:09 +0000

    first_draw_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_line.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal.
+* @n: Number of "_" characters to be printed.
+*/
+
+void print_line(int n)
+{
+int x;
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
