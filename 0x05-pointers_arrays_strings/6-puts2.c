fabc004dfee1773d00ebe97628819907f9341f16
Alexoat76 <3915@holbertonschool.com>
2021-10-12 02:03:35 +0000

    str_jump

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,11 +14,10 @@
 
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i))
-{
-_putchar(*(str + i));
-i = i + 2;
-}
+int i = 0, j = 0;
+while (str[i++])
+j++;
+for (i = 0; i < len; i += 2)
+_putchar(str[i]);
 _putchar('\n');
 }
