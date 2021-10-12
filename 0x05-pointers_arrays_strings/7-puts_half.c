7004ca5843b3cad93e2800db2cfc48e03ea71452
Alexoat76 <3915@holbertonschool.com>
2021-10-12 02:50:37 +0000

    str_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-puts_half.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* puts_half - Prints half of a string, followed by a new line.
+* @str: string to print.
+*/
+
+void puts_half(char *str)
+{
+int j, i = 0;
+while (*(str + i))
+i++;
+j = i / 2;
+if (i % 2)
+j += 1;
+while (j < i)
+{
+_putchar(*(str + j));
+j++;
+}
+_putchar('\n');
+}
