3e0ab6072d6d3f94b4beed7360ea1046d1ed607c
Alexoat76 <3915@holbertonschool.com>
2021-10-14 18:37:11 -0500

    concat_2_str_n_bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+/*
+ * File: 1-strncat.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* _strncat - Concatenates two strings.
+* @src: string to append to dest.
+* @dest: string to append by src.
+* @n: The number of bytes from src to be appended to dest.
+* Return: a pointer to the resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while (j < n && *(src + j))
+{
+*(dest + i) = *(src + j);
+i++;
+j++;
+}
+if (j < n)
+*(dest + i) = *(src + j);
+return (dest);
+}
