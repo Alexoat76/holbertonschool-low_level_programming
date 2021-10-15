5ca8dcf41e67f3e4646d16f75e50b34bdca360c2
Alexoat76 <3915@holbertonschool.com>
2021-10-14 21:34:59 -0500

    Change_to_Upp

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -17,7 +17,7 @@ char *string_toupper(char *c)
 int i = 0;
 for (i = 0; c[i] != '\0'; i++)
 {
-if (c[i] >= 'a' && l[i] <= 'z')
+if (c[i] >= 'a' && c[i] <= 'z')
 c[i] = c[i] - 'a' + 'A';
 }
 return (c);
