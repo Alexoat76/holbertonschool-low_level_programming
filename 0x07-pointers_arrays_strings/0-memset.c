4df7df5121a423e58c1cc9e64c8a00a780b1598f
Alexoat76 <3915@holbertonschool.com>
2021-10-18 16:29:33 -0500

    fills_memory_constant

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-memset.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+* _memset - function fills the first 'n' bytes of the memory area
+* pointed to by s with the constant byte b
+* @s: memory space to be filled.
+* @b: byte to be fill with.
+* @n: number of space to fill
+* Return: a pointer to the memory area s.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
