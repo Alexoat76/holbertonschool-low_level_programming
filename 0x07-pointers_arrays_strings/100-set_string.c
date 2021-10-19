0871ecc3ad5aa9b36f87993814502c1df438ca5c
Alexoat76 <3915@holbertonschool.com>
2021-10-19 17:47:06 -0500

    Double_pointer_char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,20 @@
+/*
+ * File: 100-set_string.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
+ * Return: void.
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
