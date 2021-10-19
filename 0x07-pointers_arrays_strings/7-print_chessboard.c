e60a9d1634f292498f9f51f74634d3af5324d45f
Alexoat76 <3915@holbertonschool.com>
2021-10-19 16:34:55 -0500

    Print_chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+/*
+ * File: 7-print_chessboard.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard.
+ * @a: pointer to pieces to print.
+ * Return: Void.
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
