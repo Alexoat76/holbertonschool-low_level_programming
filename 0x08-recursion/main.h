4b58a2ecf011e09e965abecf25968c8fbe31e810
Alexoat76 <3915@holbertonschool.com>
2021-10-21 18:10:30 -0500

    funct_for_0x08_Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,28 @@
+/*
+ * File: main.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+/**
+ * Desc: Header file containing prototypes for all functions,
+ *       used in the "0x04-pointers_arrays_strings" directory.
+ */
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+char *wildcard(char *s3, char *s4);
+
+#endif
