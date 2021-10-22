2ffaaf3196f1ccecb89d6a7cdefab136a6667bbc
Alexoat76 <3915@holbertonschool.com>
2021-10-22 16:49:42 -0500

    fix_funct_prime_num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,31 +7,31 @@
 #include "main.h"
 
 /**
- * is_prime_number - Determine if a number is prime
- * @n: number to evaluate
- * Return: 1 if n is prime, else 0
- */
+ * prime_check - checks to see if number is prime.
+ * @chk: check.
+ * @p: possible prime number.
+ * Return: 1 if prime, 0 if not.
+*/
+
+int prime_check(int chk, int p)
+{
+if (p < 2 || p % chk == 0)
+return (0);
+else if (chk > p / 2)
+return (1);
+else
+return (prime_check(chk + 1, p));
+}
+
+/**
+ * is_prime_number - Determine if number is prime
+ * @n: number to check
+ * Return: 1 if n is  prime, else 0.
+*/
 
 int is_prime_number(int n)
 {
-static int tester = 2;
-if (n < 2)
-{
-return (0);
-}
-else if (n < tester * 2)
-{
-tester = 2;
+if (n == 2)
 return (1);
-}
-else if (n % tester == 0)
-{
-tester = 2;
-return (0);
-}
-else
-{
-tester++;
-return (is_prime_number(n));
-}
+return (prime_check(2, n));
 }
