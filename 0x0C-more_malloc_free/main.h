09844bcb40a69a27083c983f75982f03b205affe
Alexoat76 <3915@holbertonschool.com>
2021-10-28 12:07:03 -0500

    main_more_malloc_free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,23 @@
+/*
+ * File: main.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+/**
+ * Desc: Header file containing prototypes for all functions,
+ *       used in the "0x0C-more malloc, free" directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
