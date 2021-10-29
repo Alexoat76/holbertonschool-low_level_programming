ff01ea1979538be076d817fe37ce6d91ed23abdc
Alexoat76 <3915@holbertonschool.com>
2021-10-28 20:14:53 -0500

    fixed_concat_2_str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,17 +26,19 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i]; i++)
+for (i = 0; s1[i] !='\0'; i++)
 ;
-for (j = 0; s2[j]; j++)
+for (j = 0; s2[j] !='\0'; j++)
 ;
 j > n ? (j = n) : (n = j);
-newChar = i + j + 1;
-ptr = malloc(newChar *sizeof(char));
+newChar = (i + j + 1);
+ptr = malloc(sizeof(char) * newChar);
 if (ptr == NULL)
 return (NULL);
-for (k = 0; k < newChar - 1; k++)
-k < i ? (ptr[k] = s1[k]) : (ptr[k] = s2[k - i]);
-ptr[newChar] = '\0';
+for(k=0; s1[k] !='\0'; k++)
+ptr[k]=s1[k];
+for(k=0; s2[k] !='\0'; k++)
+ptr[i+k]=s2[k];
+ptr[i+k]='\0';
 return (ptr);
 }
