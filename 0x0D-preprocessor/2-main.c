e432b221f8b7d918b18d1f3d1ee2073914ecf5e5
Alexoat76 <3915@holbertonschool.com>
2021-11-01 14:02:10 -0500

    prnt_name_file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-main.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ * Return: Alaways 0.
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
