115afa8cb600102fe17b12cf3b3fb16c40ddda50
Alexoat76 <3915@holbertonschool.com>
2021-11-01 16:52:39 -0500

    macro_ABS

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef ABS_H
+#define ABS_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ * Desc: function-like macro ABS(x) that computes the
+ *       absolute value of a number x.
+ */
+
+#define ABS(x) (((x) < 0) ? (-x) : (x))
+#endif
