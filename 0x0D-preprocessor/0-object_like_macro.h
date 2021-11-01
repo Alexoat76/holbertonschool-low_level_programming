01ec6609f5ebdca139f5719a74f41b53fc67ca82
Alexoat76 <3915@holbertonschool.com>
2021-11-01 12:33:54 -0500

    first_macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef SIZE_H
+#define SIZE_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+#endif
