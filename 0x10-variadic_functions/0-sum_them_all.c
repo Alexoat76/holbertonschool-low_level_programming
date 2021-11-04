c7e1117ea87c3b3969a37eea7a5a808238428e65
Alexoat76 <3915@holbertonschool.com>
2021-11-04 14:48:14 -0500

    fix betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,11 +18,11 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list nums;
+va_list numb;
 unsigned int i, sum = 0;
-va_start(nums, n);
+va_start(numb, n);
 for (i = 0; i < n; i++)
-sum += va_arg(nums, int);
-va_end(nums);
+sum += va_arg(numb, int);
+va_end(numb);
 return (sum);
 }
