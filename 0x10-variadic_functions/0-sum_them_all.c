0e3d4c8f942b1e779af0a6b830a95988c9c1ace5
Alexoat76 <3915@holbertonschool.com>
2021-11-04 14:42:23 -0500

    fix_code_req_5

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,11 +18,11 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list numb;
+va_list nums;
 unsigned int i, sum = 0;
-va_start(numb, n);
+va_start(nums, n);
 for (i = 0; i < n; i++)
-sum += va_arg(numb, int);
-va_end(numb);
+sum += va_arg(nums, int);
+va_end(nums);
 return (sum);
 }
