a4e91d404fc898fb330db64b6193bbbd7de44474
Alexoat76 <3915@holbertonschool.com>
2021-11-17 13:11:50 -0500

    fix betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,12 +15,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
-while (h)
-{
-nodes++;
-printf("%d\n", h->n);
-h = h->next;
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
 }
-return (nodes);
-}
+
