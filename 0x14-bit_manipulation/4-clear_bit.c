c896dd9c0e743169489712bffdeeadb386a62bc4
Alexoat76 <3915@holbertonschool.com>
2021-11-19 14:53:26 -0500

    sets the value of a bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,8 +8,8 @@
 
 /**
  * clear_bit - sets the value of a bit to 0 at a given index.
- * @n: number to set.
- * @index: index at which to set bit.
+ * @n: number to set
+ * @index: index at which to set bit
  * Return: 1 if it worked, or -1 if an error occurred.
  */
 
