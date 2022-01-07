d9423f01be4506e1392b1a3dbdddbd938d272c1f
Alexoat76 <3915@holbertonschool.com>
2022-01-06 20:30:26 -0500

    sum of all the data (n) of DLL

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/*
+ * File: 6-sum_dlistint.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+/**
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * Return: The sum of all the data.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+
+	}
+
+	return (sum);
+
+}
