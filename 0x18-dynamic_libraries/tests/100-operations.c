1f0d84b510740f13692e1188623e8e9b67d27f2a
Alexoat76 <3915@holbertonschool.com>
2022-01-10 15:35:52 -0500

    Add tests files

---
+++ 0x18-dynamic_libraries/tests/100-operations.c
@@ -0,0 +1,35 @@
+/*
+ * File: 100-operations.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+
+}
