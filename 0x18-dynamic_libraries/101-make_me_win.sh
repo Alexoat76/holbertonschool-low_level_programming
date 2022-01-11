17f655e790f584fa711d2eb49eef3b079c4512b8
Alexoat76 <3915@holbertonschool.com>
2022-01-10 22:00:30 -0500

    Crackme win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/955493bc2b37f8b005a01cb9616b82ac9a11b3e5/0x18-dynamic_libraries/mgm.so
-export LD_PRELOAD=/tmp/mgm.so
+wget -O /tmp/cracking_output.so 
+export LD_PRELOAD=/tmp/cracking_output.so
