98e463bb02394f493e6b1c634ea3a8a4e1fc324c
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-01-10 20:42:54 -0500

    Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O /tmp/crack_mega_millions.so https://github.com/Alexoat76/holbertonschool-low_level_programming/0x18-dynamic_libraries/crack_mega_millions.so
-export LD_PRELOAD=/tmp/crack_mega_millions.so
+export LD_PRELOAD=/tmp/crack_mega_millions.so ./gm
