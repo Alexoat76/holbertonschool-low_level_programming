04b0a59a2d54a9f630bcc7ce31e910cc0e605118
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-01-10 21:20:00 -0500

    Delete for fatal result

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -O /tmp/crack_mega_millions.so https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/98e463bb02394f493e6b1c634ea3a8a4e1fc324c/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/crack_mega_millions.so ./gm
