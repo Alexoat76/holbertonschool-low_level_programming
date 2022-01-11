a9668b6c0bd56371c8ccfbef4269332d2879a18f
Alexoat76 <3915@holbertonschool.com>
2022-01-10 22:56:04 -0500

    New test

--- 0x18-dynamic_libraries/prueba.sh
+++ 0x18-dynamic_libraries/prueba.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/prueba.so 
+wget -O /tmp/prueba.so https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/prueba.sh
 export LD_PRELOAD=/tmp/prueba.so
