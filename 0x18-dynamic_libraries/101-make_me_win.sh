0e2c82edf74a786042bbc23d2c4ed76ca54249d0
Alexoat76 <3915@holbertonschool.com>
2022-01-10 22:15:37 -0500

    fix code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/cracking_output.so https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/f9737ea4b1a042ac2b1b4c5970c0d7f4e4463428/0x18-dynamic_libraries/crack_mgm.so
+wget -O /tmp/crack_mgm.so https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/f9737ea4b1a042ac2b1b4c5970c0d7f4e4463428/0x18-dynamic_libraries/crack_mgm.so
 export LD_PRELOAD=/tmp/crack_mgm.so
