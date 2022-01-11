a4726049b66f9fa9bedd31ac36ad4ea860a60583
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-01-10 20:37:54 -0500

    Update script | Change URL

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/crack_mega_millions.so https://github.com/Alexoat76/holbertonschool-low_level_programming/blob/b188792c24671d94b882e2e2d296aacbdc83eeab/0x18-dynamic_libraries/crack_mega_millions.so
+wget -O /tmp/crack_mega_millions.so https://github.com/Alexoat76/holbertonschool-low_level_programming/0x18-dynamic_libraries/crack_mega_millions.so
 export LD_PRELOAD=/tmp/crack_mega_millions.so
