df46119ff6aec9d32f4644404a5d2f1908196eb3
Alexoat76 <3915@holbertonschool.com>
2022-03-24 19:44:06 -0500

    Prints| all single digit nums| base 10| start 0|putchar :heavy_check_mark:

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,8 @@
 #include <stdio.h>
 
 /**
-* main - Prints the single digit numbers of base 10
-* starting from 0
+* main - Prints all single digit numbers of base 10 starting from 0,
+*        only using putchar and without char variables.
 * Return: Always 0.
 */
 
