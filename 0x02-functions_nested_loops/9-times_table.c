df59828dcc8cd01961b322d4fb14330df5f2f43e
Alexoat76 <3915@holbertonschool.com>
2022-03-27 15:03:19 -0500

    Function| Prints the 9 times table, starting with 0 :heavy_check_mark:

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,22 @@
 
 void times_table(void)
 {
-int n, m, p;
-for (n = 0; n <= 9; n++)
-{
-_putchar('0');
-for (m = 1; m <= 9; m++)
-{
-_putchar(',');
-_putchar(' ');
-p = n * m;
-if (p <= 9)
-_putchar(' ');
-else
-_putchar((p / 10) + '0');
-_putchar((p % 10) + '0');
-}
-_putchar('\n');
-}
+	int n, m, p;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			p = n * m;
+			if (p <= 9)
+				_putchar(' ');
+			else
+				_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
