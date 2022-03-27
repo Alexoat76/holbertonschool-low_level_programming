64cd300b1e781238383b079ea87222f4b411e399
Alexoat76 <3915@holbertonschool.com>
2022-03-27 14:51:30 -0500

    Function| Prints the last digit of a number :heavy_check_mark:

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,10 @@
 
 int print_last_digit(int n)
 {
-int last_digit = n % 10;
-if (last_digit < 0)
-last_digit *= -1;
-_putchar(last_digit + '0');
-return (last_digit);
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
