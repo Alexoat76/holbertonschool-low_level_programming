d468565db471e8060307d147f7cc3504c7c1c909
Alexoat76 <3915@holbertonschool.com>
2022-03-27 14:32:36 -0500

    Function| Checks for lowercase character :heavy_check_mark:

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,13 +14,13 @@
 
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
-_putchar ('\n');
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
 }
