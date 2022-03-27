e3e74767452fde22e4d33404708304f5da74a63e
Alexoat76 <3915@holbertonschool.com>
2022-03-27 14:23:21 -0500

    Function| Prints the alphabet, in lowercase :heavy_check_mark:

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,8 +13,9 @@
 
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
-_putchar ('\n');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar ('\n');
 }
