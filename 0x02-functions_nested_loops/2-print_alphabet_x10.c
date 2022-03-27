204139390479852f24d91e63066a7a2022a87214
Alexoat76 <3915@holbertonschool.com>
2022-03-27 14:27:16 -0500

    Function| Prints 10 times the alphabet, in lowercase :heavy_check_mark:

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,18 @@
 
 /**
 * print_alphabet_x10 - Prints 10 times the alphabet in lowercase,
-* followed by a new line.
+*                      followed by a new line.
 */
 
 void print_alphabet_x10(void)
 {
-int count = 0;
-char c;
-while (count++ <= 9)
-{
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
-_putchar ('\n');
-}
+	int count = 0;
+	char c;
+
+	while (count++ <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar ('\n');
+	}
 }
