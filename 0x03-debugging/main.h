f346b2586c53c3bd1a9ebd733c4708bb353ce5de
Alexoat76 <3915@holbertonschool.com>
2022-03-28 18:18:08 -0500

    Add header file for this project :heavy_check_mark:

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdio.h>
+
+/*
+ * File: main.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ * Desc: Header file containing prototypes for all functions
+ *       in the 0x03. C - Debugging| project.
+ */
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
