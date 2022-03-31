3582df29743e0bedb6976f05f1b6147af42f465e
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:38:16 -0500

    Program| Finds and prints| largest prime factor of the number :heavy_check_mark:

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,13 +14,14 @@
 
 int main(void)
 {
-long i;
-long num = 612852475143;
-for (i = 2; i < num; i++)
-{
-if (num % i == 0)
-num /= i;
-}
-printf("%li\n", num);
-return (0);
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
 }
