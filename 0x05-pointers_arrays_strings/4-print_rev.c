e98fb8d255ff548e66bbd3c91c7fb54a6e4f9039
Alexoat76 <3915@holbertonschool.com>
2022-03-31 11:12:30 -0500

    Function| Prints a string, in reverse :heavy_check_mark:

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,15 @@
 
 void print_rev(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-}
-for (i = i - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
