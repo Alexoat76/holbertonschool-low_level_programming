3d452f4cde0aec23b6cecf8c72c5ad9a8f406f45
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:28:34 -0500

    Function| Prints the numbers from 0 to 9 :heavy_check_mark:

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -14,8 +14,10 @@
 
 void print_numbers(void)
 {
-int n;
-for (n = 0; n <= 9; n++)
-_putchar((n % 10) + '0');
-_putchar('\n');
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar((n % 10) + '0');
+
+	_putchar('\n');
 }
