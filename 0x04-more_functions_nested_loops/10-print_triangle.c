b3c65d800233be1898bd4b7f6e2596b71c1c63b7
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:37:04 -0500

    Function| Prints a triangle :heavy_check_mark:

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,27 +14,28 @@
 
 void print_triangle(int size)
 {
-int i, j;
-if (size > 0)
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size; j++)
-{
-if (j <= size - i)
-{
-_putchar(' ');
-}
-else
-{
-_putchar('#');
-}
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
