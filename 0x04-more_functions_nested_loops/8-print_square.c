74f4933c264539af41e58d4d0481af4860608226
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:33:50 -0500

    Function| Prints a square with # :heavy_check_mark:

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,20 +14,21 @@
 
 void print_square(int size)
 {
-int i, j;
-if (size > 0)
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
