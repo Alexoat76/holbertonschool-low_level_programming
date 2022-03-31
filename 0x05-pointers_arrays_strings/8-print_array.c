8aeaa28e3f459e1702ad4938f808e2b3a28a53bd
Alexoat76 <3915@holbertonschool.com>
2022-03-31 11:21:56 -0500

    Function| Prints n elements of an array of integers :heavy_check_mark:

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,14 @@
 
 void print_array(int *a, int n)
 {
-int i = 0;
-while (i < n)
-{
-printf("%d", a[i]);
-if (i < (n - 1))
-printf(", ");
-i++;
-}
-printf("\n");
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 }
