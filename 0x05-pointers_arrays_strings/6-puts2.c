b3caf4fdd0f38e7c97bd2481f7415ee8a82b0092
Alexoat76 <3915@holbertonschool.com>
2022-03-31 11:17:55 -0500

    Function| Prints every other character| String, start first character :heavy_check_mark:

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,11 @@
 
 void puts2(char *str)
 {
-int i = 0, j = 0;
-while (str[i++])
-j++;
-for (i = 0; i < j; i += 2)
-_putchar(str[i]);
-_putchar('\n');
+	int i = 0, j = 0;
+
+	while (str[i++])
+		j++;
+	for (i = 0; i < j; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
 }
