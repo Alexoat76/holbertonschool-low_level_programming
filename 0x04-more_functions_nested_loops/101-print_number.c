1a1444fca6003e3ac190c0d33e32579b803c319b
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:41:47 -0500

    Function| Prints an integer with conditions :heavy_check_mark:

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,13 +13,14 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
