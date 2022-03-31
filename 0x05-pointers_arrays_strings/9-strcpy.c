5245ada6deb97f5c0bcb19a16df7ec30b90dd8a0
Alexoat76 <3915@holbertonschool.com>
2022-03-31 11:26:25 -0500

    Function| Copies str pointed to by src to the buffer pointed to by dest :heavy_check_mark:

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,11 +17,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
-for ( ; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = '\0';
-return (dest);
+	int i = 0;
+
+	for ( ; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
