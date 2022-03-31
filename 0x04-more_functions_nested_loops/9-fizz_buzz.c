fb103ac4009d82f76547704940b70dfc43f35712
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:35:47 -0500

    Program| Prints the numbers from 1 to 100| Fizz-Buzz :heavy_check_mark:

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,29 +16,30 @@
 
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 || i % 5 == 0)
-{
-if (i % 3 == 0)
-{
-printf("Fizz");
-}
-if (i % 5 == 0)
-{
-printf("Buzz");
-}
-}
-else
-{
-printf("%d", i);
-}
-if (i != 100)
-{
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
