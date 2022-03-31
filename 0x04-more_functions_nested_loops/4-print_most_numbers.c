32bf8403cf8dae10295967cac17d7236ff297454
Alexoat76 <3915@holbertonschool.com>
2022-03-30 20:29:15 -0500

    Function| Prints the numbers from 0 to 9 :heavy_check_mark:

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,11 +13,12 @@
 
 void print_most_numbers(void)
 {
-int n;
-for (n = 0; n <= 9; n++)
-{
-if (n != 2 && n != 4)
-_putchar((n % 10) + '0');
-}
-_putchar('\n');
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
 }
