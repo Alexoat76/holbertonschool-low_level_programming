5a574176b09ed664814ac2815aba970dad6f4128
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:51:33 -0500

    Function| Copies a string :heavy_check_mark:

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,17 +16,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-i = 0;
-while (i < n && *(src + i))
-{
-*(dest + i) = *(src + i);
-i++;
-}
-while (i < n)
-{
-*(dest + i) = '\0';
-i++;
-}
-return (dest);
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
 }
