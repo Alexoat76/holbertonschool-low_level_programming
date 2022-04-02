f09c511187395132a9c044ae32d05f6055710267
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:54:57 -0500

    Function| Changes all lowercase letters of a string to uppercase :heavy_check_mark:

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,11 +14,12 @@
 
 char *string_toupper(char *c)
 {
-int i = 0;
-for (i = 0; c[i] != '\0'; i++)
-{
-if (c[i] >= 'a' && c[i] <= 'z')
-c[i] = c[i] - 'a' + 'A';
-}
-return (c);
+	int i = 0;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 'a' + 'A';
+	}
+	return (c);
 }
