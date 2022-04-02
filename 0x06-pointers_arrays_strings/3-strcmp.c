918f574876dfe509e1a29d8724f30d92817b0b0d
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:52:33 -0500

    Function| Compares two strings :heavy_check_mark:

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,8 +15,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
-i++;
-return (*(s1 + i) -*(s2 + i));
+	int i = 0;
+
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+		i++;
+	return (*(s1 + i) - *(s2 + i));
 }
