958a128842050d953e4158051ebf123240e1c47a
Alexoat76 <3915@holbertonschool.com>
2022-04-02 11:58:18 -0500

    Function| Locates a substring :heavy_check_mark:

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,21 +16,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
-i = 0;
-if (haystack[i] == needle[i])
-{
-do {
-if (needle[i + 1] == '\0')
-return (haystack);
-i++;
-} while (haystack[i] == needle[i]);
-}
-haystack++;
-}
-return ('\0');
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
 }
