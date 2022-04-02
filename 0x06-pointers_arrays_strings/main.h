1763328291cc2913fc69ee460f88d19794efef34
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:46:30 -0500

    Add header file for project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,15 +4,16 @@
  * email: 3915@holbertonschool.com
  */
 
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdlib.h>
 
 /**
  * Desc: Header file containing prototypes for all functions,
- *       used in the "0x04-pointers_arrays_strings" directory.
+ *       used in the 0x06. C - More pointers, arrays and strings| project.
  */
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -21,5 +22,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-#endif
+#endif  /* _MAIN_H_ */
