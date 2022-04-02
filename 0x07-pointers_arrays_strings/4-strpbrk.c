295e5b4236ac64375ba53141d17c3b456b945ced
Alexoat76 <3915@holbertonschool.com>
2022-04-02 11:57:31 -0500

    Function| Searches a string for any of a set of bytes :heavy_check_mark:

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,15 +16,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i;
-while (*s != '\0')
-{
-for (i = 0; accept[i] != '\0'; i++)
-{
-if (*s == accept[i])
-return (s);
-}
-s++;
-}
-return (0);
+	unsigned int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
 }
