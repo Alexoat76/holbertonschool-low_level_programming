328077997ab06b33f002f4bc1d21b28a9ce2ddf1
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:58:41 -0500

    Function| Encodes a string into 1337 :heavy_check_mark:

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,16 +14,17 @@
 
 char *leet(char *str)
 {
-int i, j;
-char a[] = "aAeEoOtTlL";
-char b[] = "4433007711";
-for (i = 0; *(str + i); i++)
-{
-for (j = 0; j <= 9; j++)
-{
-if (a[j] == *(str + i))
-*(str + i) = b[j];
-}
-}
-return (str);
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(str + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(str + i))
+				*(str + i) = b[j];
+		}
+	}
+	return (str);
 }
