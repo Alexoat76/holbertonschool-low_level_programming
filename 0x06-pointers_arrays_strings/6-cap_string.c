94d829e535c9cefe0ab799d7ba159d5d4a2fd5cd
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:55:55 -0500

    Function| Capitalizes all words of a string :heavy_check_mark:

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,24 +14,25 @@
 
 char *cap_string(char *c)
 {
-int i = 0, j;
-char x[] = " \t\n,;.!?\"(){}";
-while (*(c + i))
-{
-if (*(c + i) >= 'a' && *(c + i) <= 'z')
-{
-if (i == 0)
-*(c + i) -= 'a' - 'A';
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (x[j] == *(c + i - 1))
-*(c + i) -= 'a' - 'A';
-}
-}
-}
-i++;
-}
-return (c);
+	int i = 0, j;
+	char x[] = " \t\n,;.!?\"(){}";
+
+	while (*(c + i))
+	{
+		if (*(c + i) >= 'a' && *(c + i) <= 'z')
+		{
+			if (i == 0)
+				*(c + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (x[j] == *(c + i - 1))
+						*(c + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (c);
 }
