0c8d046a79c5b5af79f8d899149af86fbeb34375
Alexoat76 <3915@holbertonschool.com>
2022-04-01 23:50:34 -0500

    Function| Concatenates two strings :heavy_check_mark:

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,17 +16,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
-i = j = 0;
-while (*(dest + i))
-i++;
-while (j < n && *(src + j))
-{
-*(dest + i) = *(src + j);
-i++;
-j++;
-}
-if (j < n)
-*(dest + i) = *(src + j);
-return (dest);
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
 }
