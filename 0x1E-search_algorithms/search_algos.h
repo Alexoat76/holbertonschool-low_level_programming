770e0546a145c4453d07e68e5a31cd53e7773e6e
Alexoat76 <3915@holbertonschool.com>
2022-06-11 10:44:20 -0500

    Add Header file | :heavy_check_mark:

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,20 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * File: search_algos.h
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ * Desc: Header file containing structures and prototypes
+ *       for all functions in the project 0x1E. Search algorithms.
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALGOS_H */
